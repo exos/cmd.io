abefd1f6e9de7eccc78bc69c180cb0c78b5b3944
exos <oscar@gentisoft.com>
2012-07-02 05:19:48 -0300

    Primera version

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 
 module.exports = {
-    io: require('./lib/cmdio')
+    io: require('./lib/cmdio'),
+    
+    clientExpressRoute: function (eapp) {
+        eapp.get('/cmdio/client.js', function (req, res) {
+            
+            var stream = require('fs').createReadStream(__dirname+'/lib/cmdio.js');
+            
+            res.header('Content-Type', 'text/javascript');
+            
+            stream.on('error', function (data) {
+                res.end('Error loading file');
+            });
+            
+            stream.on('data', function (data) {
+                res.write(data);
+            });
+            
+            stream.on('end', function (data) {
+                res.end();
+                stream = null;
+            });
+            
+        });
+    }
+
 };
